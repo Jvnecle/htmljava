075447f6cbe2ed9000744936b108fc7c850c03de
Jvnecle <161715333+Jvnecle@users.noreply.github.com>
2024-10-24 08:01:47 -0300

    Add files via upload

--- Principal.js
+++ Principal.js
@@ -9,6 +9,23 @@ var peso = tdPeso.textContent
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / altura * altura; // 100 / 2.0 * 2.0 = 100 / 4=>>>>> 25
+var tdImc = paciente.querySelector(".info-imc");
 
-console.log(imc);
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso < 0 || peso > 1000){
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido"
+}
+
+if(altura < 0 || altura > 3.00){
+    console.log("Altura inválida")
+    alturaEhValida = false;
+}
+
+if( alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura); 
+    tdImc.textContent = imc;
+}
